c35a9a6ca5fd8a66b933875283d8acfc6138c5b6
BMonikaB <boreckamonika39@gmail.com>
2025-04-04 12:25:29 +0200

    Add Main class with date from task file

---
+++ src/main/java/com/scoreboard/Main.java
@@ -0,0 +1,28 @@
+package com.scoreboard;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ScoreBoard board = new ScoreBoard();
+
+        board.startGame("Mexico", "Canada");
+        board.updateScore("Mexico", "Canada", 0, 5);
+
+        board.startGame("Spain", "Brazil");
+        board.updateScore("Spain", "Brazil", 10, 2);
+
+        board.startGame("Germany", "France");
+        board.updateScore("Germany", "France", 2, 2);
+
+        board.startGame("Uruguay", "Italy");
+        board.updateScore("Uruguay", "Italy", 6, 6);
+
+        board.startGame("Argentina", "Australia");
+        board.updateScore("Argentina", "Australia", 3, 1);
+
+        System.out.println("Match Summary:");
+        for (Match match : board.getSummary()) {
+            System.out.println(match);
+        }
+    }
+}
